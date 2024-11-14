5dd91062b622b4efed5e4ac5d4eeeb7b567ce2fa
sutaC <catus.pl@gmail.com>
2024-11-14 12:24:55 +0100

    Get abs path

--- disk/system/utils/kernel.py
+++ disk/system/utils/kernel.py
@@ -4,6 +4,7 @@ class Kernel:
     def __init__(self) -> None:
         print("Kernel connecting...")
         self.__conn: sqlite3.Connection = sqlite3.connect("filesystem.db") 
+        self.__node: int = 1
         # Inits database
         cursor = self.__conn.cursor()
         # Creates tables
@@ -25,14 +26,19 @@ class Kernel:
             )
         ''')
         self.__conn.commit() # Adds tables if empty
-        ROOT = cursor.execute('''
+        root = cursor.execute('''
             SELECT id FROM nodes WHERE parent_id IS NULL
         ''').fetchone()
-        if ROOT is None: # Adds root directory if doesn't exist
+        print()
+        if root is None: # Adds root directory if doesn't exist
             cursor.execute('''
                 INSERT INTO nodes (id, name, parent_id, type) VALUES (NULL, 'root', NULL, 'directory')               
             ''')
             self.__conn.commit()  
+            root = cursor.execute('''
+                SELECT id FROM nodes WHERE parent_id IS NULL
+            ''').fetchone()
+        self.__node = root[0]
         cursor.close()
         print("Kernel connected")
 
@@ -40,3 +46,14 @@ class Kernel:
         print("Closing kernel connection...")
         self.__conn.close()
         print("Kernel connection closed")
+
+    # Public
+    def get_abs_path(self, id: int = None) -> str: 
+        if id is None:
+            return "/"
+        cursor = self.__conn.cursor()
+        node = cursor.execute("SELECT name, parent_id FROM nodes WHERE id = ?", [id]).fetchone()
+        cursor.close()
+        if node[0] == 'root':
+            return "/"
+        return self.get_abs_path(node[1]) + node[0] + "/"
